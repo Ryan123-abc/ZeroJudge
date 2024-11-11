bdea89d05bcb930610161645c04681d4ecd00bfa
Ryan123-abc <ryan950109@gmail.com>
2024-11-11 16:48:21 +0800

    upd

---
+++ a006.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+	int a, b, c;
+	scanf("%d %d %d", &a, &b, &c);
+
+	int D = b*b-4*a*c;
+	if( D < 0)
+		printf("No real root\n");
+	else{
+		int x1 = (-b+sqrt(D))/(2*a);
+		int x2 = (-b-sqrt(D))/(2*a);
+
+		if( D>0 )
+			printf("Two different roots x1=%d, x2=%d", x1, x2);
+		else if(D == 0)
+			printf("Two same roots x=%d", x1);
+	}
+	return 0;
+}
+
+
