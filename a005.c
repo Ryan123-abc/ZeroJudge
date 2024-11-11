76eae6ae948a24f3250c45ced49669157627d8c3
Ryan123-abc <ryan950109@gmail.com>
2024-11-11 16:12:42 +0800

    upd

--- a005.c
+++ a005.c
@@ -3,10 +3,14 @@
 int main(){
 	int a;
 	scanf("%d", &a);
-	int b, c, d, e, f;
-	for(int i=1; i<=a; i++){
-		scanf("%d %d %d %d", &b, &c, &d, &e);
-		f = c/b;
-		printf("%d %d %d %d %d", b, c, d, e, e*f);
+	for(int i=0; i<a; i++){
+		int b[4];
+		scanf("%d %d %d %d", &b[0], &b[1], &b[2], &b[3]);
+		if(b[1]-b[0]==b[2]-b[1])
+			printf("%d %d %d %d %d\n", b[0], b[1], b[2], b[3], b[3]+(b[1]-b[0]));
+		else
+			printf("%d %d %d %d %d\n", b[0], b[1], b[2], b[3], b[3]*(b[1]-b[0]));
+		
 	}
+	return 0;
 }
