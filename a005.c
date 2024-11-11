464f2351fc5ee099e3fe78fcdad91d8d04138759
Ryan123-abc <ryan950109@gmail.com>
2024-11-11 16:16:57 +0800

    upd

--- a005.c
+++ a005.c
@@ -9,7 +9,7 @@ int main(){
 		if(b[1]-b[0]==b[2]-b[1])
 			printf("%d %d %d %d %d\n", b[0], b[1], b[2], b[3], b[3]+(b[1]-b[0]));
 		else
-			printf("%d %d %d %d %d\n", b[0], b[1], b[2], b[3], b[3]*(b[1]-b[0]));
+			printf("%d %d %d %d %d\n", b[0], b[1], b[2], b[3], b[3]*(b[1]/b[0]));
 		
 	}
 	return 0;
